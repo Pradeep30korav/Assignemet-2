724973a9dbdf71964ea5a699994b43931fca397e
Pradeep30korav <121960614+Pradeep30korav@users.noreply.github.com>
2023-01-20 20:01:27 +0530

    Assignment2 question 2

---
+++ _2_Assignemet_2.py
@@ -0,0 +1,10 @@
+
+pradeep_dict = {}
+ 
+# Now populate it
+for i in range(97, 97 + 26):
+    # Map character to ascii value
+    pradeep_dict[chr(i)] = i
+ 
+# Print the populated dictionary
+print(pradeep_dict)
